fadd7f69196e9cd1581d4031560297e342c10405
Atsuki02 <canvanvam@gmail.com>
2023-06-04 14:35:38 -0700

    completed

--- script.js
+++ script.js
@@ -22,6 +22,9 @@ const config = {
 };
 
 class BankAccount {
+  maxWithdrawPercent = 0.2;
+  annualInterestRate = 0.08;
+
   constructor(firstName, lastName, email, type, accountNumber, money) {
     this.firstName = firstName;
     this.lastName = lastName;
@@ -34,6 +37,31 @@ class BankAccount {
 
   getFullName() {
     return this.firstName + " " + this.lastName;
+  }
+
+  calculateWithdrawAmount(amount) {
+    let maxWithdrawAmount = Math.floor(this.money * this.maxWithdrawPercent);
+    amount = amount > maxWithdrawAmount ? maxWithdrawAmount : amount;
+    return amount;
+  }
+
+  withdraw(amount) {
+    this.money -= this.calculateWithdrawAmount(amount);
+    return this.money;
+  }
+
+  deposit(amount) {
+    this.money += amount;
+    return amount;
+  }
+
+  simulateTimePassage(days) {
+    let daysPerYear = 365;
+    let profit =
+      this.money * Math.pow(1 + this.annualInterestRate, days / daysPerYear) -
+      this.money;
+    this.money += profit;
+    return profit;
   }
 }
 
@@ -77,9 +105,7 @@ function mainBankPage(userBankAccount) {
           </div>
           <div class="available-balance">
             <p class="available-balance-title">Available Balance</p>
-            <p class="available-balance-amount">$${
-              userBankAccount.initialDeposit
-            }</p>
+            <p class="available-balance-amount">$${userBankAccount.money}</p>
           </div>
           <div class="items">
             <div class="withdrawal">
@@ -102,17 +128,20 @@ function mainBankPage(userBankAccount) {
   mainPage
     .querySelectorAll(".withdrawal")[0]
     .addEventListener("click", function () {
-      withdrawController(userBankAccount);
+      sideBankSwitch();
+      config.sidePage.append(withdrawPage(userBankAccount));
     });
   mainPage
     .querySelectorAll(".deposit")[0]
     .addEventListener("click", function () {
-      alert("deposit");
+      sideBankSwitch();
+      config.sidePage.append(depositPage(userBankAccount));
     });
   mainPage
     .querySelectorAll(".comebacklater")[0]
     .addEventListener("click", function () {
-      alert("comebacklater");
+      sideBankSwitch();
+      config.sidePage.append(comeBackLaterPage(userBankAccount));
     });
 
   return mainPage;
@@ -165,19 +194,32 @@ function backNextBtn(backString, nextString) {
   container.innerHTML = `
           <div class="next-page">
             <button class="go-back back-btn">${backString}</button>
-            <button class="next">${nextString}</button>
+            <button class="next next-btn">${nextString}</button>
           </div>
   `;
   return container;
 }
 
-function withdrawController(userBankAccount) {
+function sideBankSwitch() {
   displayNone(config.bankPage);
   displayBlock(config.sidePage);
   config.bankPage.innerHTML = "";
   config.sidePage.innerHTML = "";
-  config.sidePage.append(withdrawPage(userBankAccount));
 }
+
+function bankReturn(userBankAccount) {
+  displayNone(config.sidePage);
+  displayBlock(config.bankPage);
+  config.bankPage.append(mainBankPage(userBankAccount));
+}
+
+// function withdrawController(userBankAccount) {
+//   displayNone(config.bankPage);
+//   displayBlock(config.sidePage);
+//   config.bankPage.innerHTML = "";
+//   config.sidePage.innerHTML = "";
+//   config.sidePage.append(withdrawPage(userBankAccount));
+// }
 
 function withdrawPage(userBankAccount) {
   let container = document.createElement("div");
@@ -192,9 +234,10 @@ function withdrawPage(userBankAccount) {
 
   let backBtn = withdrawContainer.querySelectorAll(".back-btn").item(0);
   backBtn.addEventListener("click", function () {
-    displayNone(config.sidePage);
-    displayBlock(config.bankPage);
-    config.bankPage.append(mainBankPage(userBankAccount));
+    // displayNone(config.sidePage);
+    // displayBlock(config.bankPage);
+    // config.bankPage.append(mainBankPage(userBankAccount));
+    bankReturn(userBankAccount);
   });
 
   let billInputs = withdrawContainer.querySelectorAll(".bill-input");
@@ -205,8 +248,50 @@ function withdrawPage(userBankAccount) {
       `;
     });
   }
-
   console.log(billInputs);
+
+  let nextBtn = withdrawContainer.querySelectorAll(".next-btn").item(0);
+  nextBtn.addEventListener("click", function () {
+    container.innerHTML = "";
+    container.setAttribute("id", "page4");
+    let confirmDialog = document.createElement("div");
+    confirmDialog.classList.add("confirmation-page");
+    confirmDialog.append(
+      billDialog(
+        "The money you are going to take is ...",
+        billInputs,
+        "data-bill"
+      )
+    );
+    container.append(confirmDialog);
+    let total = billSummation(billInputs, "data-bill");
+    confirmDialog.innerHTML += `
+    <div class="available-amount">
+      <p class="available-amount-title">Total to be withdrawn:</p>
+      <p class="available-amount-total">$${userBankAccount.calculateWithdrawAmount(
+        total
+      )}</p>
+    </div>
+    `;
+    let withdrawConfirmBtns = backNextBtn("Go Back", "Confirm");
+    confirmDialog.append(withdrawConfirmBtns);
+
+    let confirmBackBtn = withdrawConfirmBtns.querySelectorAll(".back-btn")[0];
+    let confirmNextBtn = withdrawConfirmBtns.querySelectorAll(".next-btn")[0];
+
+    confirmBackBtn.addEventListener("click", function () {
+      container.innerHTML = "";
+      container.append(withdrawContainer);
+    });
+
+    confirmNextBtn.addEventListener("click", function () {
+      userBankAccount.withdraw(total);
+      // displayNone(config.sidePage);
+      // displayBlock(config.bankPage);
+      // config.bankPage.append(mainBankPage(userBankAccount));
+      bankReturn(userBankAccount);
+    });
+  });
 
   return container;
 }
@@ -223,4 +308,145 @@ function billSummation(inputElementNodeList, multiplierAttribute) {
     if (value > 0) summation += value;
   }
   return summation;
+}
+
+function billDialog(title, inputElementNodeList, multiplierAttribute) {
+  let container = document.createElement("div");
+
+  let billElements = "";
+  for (let i = 0; i < inputElementNodeList.length; i++) {
+    let value = parseInt(inputElementNodeList[i].value);
+    if (value > 0) {
+      let bill =
+        "$" + inputElementNodeList[i].getAttribute(multiplierAttribute);
+      billElements += `
+            <div class="confirmation-section">
+              <p class="confirmation-unit">${value} × ${bill}</p>
+            </div>
+        `;
+    }
+  }
+  let totalString = `
+    <div class="confirmation-section">
+    <p class="confirmation-total">total: $${billSummation(
+      inputElementNodeList,
+      multiplierAttribute
+    )}</p>
+    </div>
+  `;
+  container.innerHTML = `
+          <div class="confirmation-title">
+            <h1 class="">${title}</h1>
+          </div>
+          <div class="confirmation-amount-money">
+          ${billElements}
+          ${totalString}
+          </div>  
+  `;
+  return container;
+}
+
+function depositPage(userBankAccount) {
+  let container = document.createElement("div");
+  container.setAttribute("id", "page3");
+  let depositContainer = document.createElement("div");
+  depositContainer.classList.add("withdrawal-page");
+  container.append(depositContainer);
+  depositContainer.append(billInputSelector("Please Enter The Deposit Amount"));
+  depositContainer.append(backNextBtn("back", "next"));
+
+  let backBtn = depositContainer.querySelectorAll(".back-btn").item(0);
+  backBtn.addEventListener("click", function () {
+    // displayNone(config.sidePage);
+    // displayBlock(config.bankPage);
+    // config.bankPage.append(mainBankPage(userBankAccount));
+    bankReturn(userBankAccount);
+  });
+
+  let billInputs = depositContainer.querySelectorAll(".bill-input");
+  for (let i = 0; i < billInputs.length; i++) {
+    billInputs[i].addEventListener("change", function () {
+      document.querySelector(".total-amount").innerHTML = `
+      $${billSummation(billInputs, "data-bill")}
+      `;
+    });
+  }
+
+  let nextBtn = depositContainer.querySelectorAll(".next-btn").item(0);
+  nextBtn.addEventListener("click", function () {
+    container.innerHTML = "";
+    container.setAttribute("id", "page4");
+    let confirmDialog = document.createElement("div");
+    confirmDialog.classList.add("confirmation-page");
+    confirmDialog.append(
+      billDialog(
+        "The money you are going to deposit is ...",
+        billInputs,
+        "data-bill"
+      )
+    );
+    container.append(confirmDialog);
+    let total = billSummation(billInputs, "data-bill");
+    confirmDialog.innerHTML += `
+    <div class="available-amount">
+      <p class="available-amount-title">Total to be depsited:</p>
+      <p class="available-amount-total">$${userBankAccount.calculateWithdrawAmount(
+        total
+      )}</p>
+    </div>
+    `;
+    let depositConfirmBtns = backNextBtn("Go Back", "Confirm");
+    confirmDialog.append(depositConfirmBtns);
+
+    let confirmBackBtn = depositConfirmBtns.querySelectorAll(".back-btn")[0];
+    let confirmNextBtn = depositConfirmBtns.querySelectorAll(".next-btn")[0];
+
+    confirmBackBtn.addEventListener("click", function () {
+      container.innerHTML = "";
+      container.append(depositContainer);
+    });
+
+    confirmNextBtn.addEventListener("click", function () {
+      userBankAccount.deposit(total);
+      // displayNone(config.sidePage);
+      // displayBlock(config.bankPage);
+      // config.bankPage.append(mainBankPage(userBankAccount));
+      bankReturn(userBankAccount);
+    });
+  });
+
+  return container;
+}
+
+function comeBackLaterPage(userBankAccount) {
+  let container = document.createElement("div");
+  container.setAttribute("id", "page6");
+  comeBackContainer = document.createElement("div");
+  comeBackContainer.classList.add("days-page");
+  container.append(comeBackContainer);
+  comeBackContainer.innerHTML = `
+          <div class="days-title">
+            <h1 class="">How many days will you be gone?</h1>
+          </div>
+          <div class="days-amount">
+            <input id="days-gone" type="number" name="days" placeholder="0 days" />
+          </div>
+  `;
+  comeBackContainer.append(backNextBtn("Back", "Confirm"));
+
+  let backBtn = container.querySelectorAll(".back-btn")[0];
+  backBtn.addEventListener("click", function () {
+    bankReturn(userBankAccount);
+  });
+
+  let nextBtn = container.querySelectorAll(".next-btn")[0];
+  nextBtn.addEventListener("click", function () {
+    let daysGoneInput = container.querySelectorAll("#days-gone")[0];
+    let totalDaysGone = parseInt(daysGoneInput.value);
+    totalDaysGone = totalDaysGone > 0 ? totalDaysGone : 0;
+    userBankAccount.simulateTimePassage(totalDaysGone);
+    bankReturn(userBankAccount);
+  });
+
+  return container;
 }
